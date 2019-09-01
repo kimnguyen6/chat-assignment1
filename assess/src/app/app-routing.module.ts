78c5f5b001814c886ee4d6a0095536240a7927f7
Kim Nguyen <kim.nguyen8@griffithuni.edu.au>
2019-09-01 20:10:48 +1000

    tested connection with login to dashboard

--- assess/src/app/app-routing.module.ts
+++ assess/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { GroupComponent } from './group/group.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChannelComponent } from './channel/channel.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'group', component: GroupComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'channel', component: ChannelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- assess/src/app/login/login.component.ts
+++ assess/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  username: string = "";
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    localStorage.clear();
   }
 
+  login(){
+    if(this.username === ""){
+      alert("Username field is empty. Please fill in field");
+    } else {
+      localStorage.setItem("username", this.username);
+      this.router.navigateByUrl('/dashboard');
+    }
+  }
 }
